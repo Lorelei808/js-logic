178e686bdeb0dc27f795c742c0ef9beeac8abf5a
unknown <lori.darlow@gmail.com>
2018-06-09 17:47:18 -1000

    #9 js-logic

--- exercises.js
+++ exercises.js
@@ -196,8 +196,15 @@ console.log(spareChange(100.1));
  * The function will return true if the sum of all the number values is greater than 30, otherwise it will return false.
  * Console.log your result.
 */ 
-
-
+console.log("9--------------------");
+function dirty30(one, two, three){
+  if(one + two + three > 30){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log(dirty30(4, 3, 22));
 
 /*
  * #10
